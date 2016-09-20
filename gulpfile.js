d49040db7e1d1bf795f6e7d062b6e08408dce266
GLOBANT\faviola.narvaez <faviola.narvaez@globant.com>
2016-09-20 14:01:37 -0500

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,21 @@ var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
 var typescript = require('gulp-tsc');
+var runSequence = require('run-sequence');
+var sourcemaps = require('gulp-sourcemaps');
+var tsc = require('gulp-typescript');
+var tslint = require('gulp-tslint');
+var uglify = require('gulp-uglify');
+var tsProject = tsc.createProject('tconfig.json');
+var tsTestProject = tsc.createProject('tconfig.json');
+var rimraf = require('gulp-rimraf');
+var minifyHTML = require('gulp-htmlmin');
+var templates = require('gulp-angular-templatecache');
+var inject = require('gulp-inject');
+var ngAnnotate = require('gulp-ng-annotate');
+var addStream = require('add-stream');
+var Karma = require('karma').Server;
+var path = require('path');
 
 var paths = {
     sass: ['./scss/**/*.scss'],
@@ -22,6 +37,38 @@ gulp.task('compile', function() {
         emitError: false
     }))
     .pipe(gulp.dest('./www/js/'))
+});
+
+gulp.task('lint', function () {
+    return gulp.src([
+      'source/**.ts'
+    ])
+      .pipe(tslint({}))
+      .pipe(tslint.report('verbose'));
+});
+
+gulp.task('compile-app', function () {
+    var libraryName = 'ionicSideMenu';
+    var outputFileName = libraryName + '.min.js';
+
+    return gulp.src([
+      'source/**.ts',
+      'www/data/*.json',
+      'typings/tsd.d.ts/'
+    ])
+      .pipe(sourcemaps.init())
+      .pipe(tsc(tsProject))
+      .pipe(addStream.obj(prepareTemplates()))
+      .pipe(concat(outputFileName))
+      .pipe(ngAnnotate())
+      .pipe(uglify())
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('www/js'));
+});
+
+gulp.task('clean', function () {
+    return gulp.src(['www/js', 'www/css'], { read: false })
+      .pipe(rimraf());
 });
 
 gulp.task('sass', function(done) {
@@ -61,3 +108,14 @@ gulp.task('git-check', function(done) {
   }
   done();
 });
+
+function prepareTemplates() {
+    return gulp.src(['./www/templates/**/**/*.html', './www/templates/*.html', , '!./www/index.html'])
+      .pipe(minifyHTML({
+          collapseWhitespace: true
+      }))
+      .pipe(templates('templates.js', {
+          module: 'sidemenu.templates',
+          standalone: true
+      }));
+}
